1cb09c47354927bcabe3ca694cc3870496cad192
ramyij <rij2105@columbia.edu>
2018-04-12 20:11:59 -0400

    added co2 sensor code

--- iotProject.py
+++ iotProject.py
@@ -12,6 +12,10 @@ from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex, GlobalAllInd
 from boto.dynamodb2.types import NUMBER
 import mraa
 import pyupm_i2clcd as lcd
+from __future__ import print_function
+import time, sys, signal, atexit
+from upm import pyupm_mg811 as sensorObj
+
 
 DYNAMO_TABLE_NAME ='iotProject'
 ACCOUNT_ID ='163430531586'
@@ -25,7 +29,7 @@ oidc = cognito.get_open_id_token(cognito_id['IdentityId'])
 assumedRoleObject = sts.assume_role_with_web_identity(ROLE_ARN, "XX", oidc['Token'])
 
 
-
+co2Sensor = sensorObj.MG811(0, 6, 5.0)
 
 
 client_dynamo = boto.dynamodb2.connect_to_region(
@@ -36,6 +40,14 @@ client_dynamo = boto.dynamodb2.connect_to_region(
 table_dynamo = Table(DYNAMO_TABLE_NAME,connection=client_dynamo)
 
 
+# This function lets you run code on exit
+def exitHandler():
+    print("Exiting")
+    sys.exit(0)
+
+# Register exit handlers
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
 
 ## Read Sensor Values
 try:
@@ -58,10 +70,12 @@ while(1):
                             'timestamp':str(datetime.now()),
                             'GasSensor':gasSensor.read(),
                             'DustSensor': dustSensor.read(),
+                            'CO2': co2Sensor.ppm()
                             'room': str(room)
                         })
         print "Dust: " + str(dustSensor.read())
         print "Gas: " + str(gasSensor.read())
+        print "CO2: " + str(co2Sensor.ppm())
         time.sleep(5)
     except:
         time.sleep(5)
