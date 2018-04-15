a8aad1dc0524aa9a384001853ff8ac0560434c70
root <rij2105@columbia.edu>
2018-04-15 16:08:21 +0000

    updated python code

--- iotProject.py
+++ iotProject.py
@@ -11,11 +11,7 @@ from boto.dynamodb2.table import Table
 from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex, GlobalAllIndex
 from boto.dynamodb2.types import NUMBER
 import mraa
-import pyupm_i2clcd as lcd
-from __future__ import print_function
-import time, sys, signal, atexit
 from upm import pyupm_mg811 as sensorObj
-
 
 DYNAMO_TABLE_NAME ='iotProject'
 ACCOUNT_ID ='163430531586'
@@ -28,9 +24,10 @@ cognito_id = cognito.get_id(ACCOUNT_ID, IDENTITY_POOL_ID)
 oidc = cognito.get_open_id_token(cognito_id['IdentityId'])
 assumedRoleObject = sts.assume_role_with_web_identity(ROLE_ARN, "XX", oidc['Token'])
 
-
-# co2Sensor = sensorObj.MG811(0, 6, 5.0)
-
+co2Sensor = sensorObj.MG811(0,2,5.0)
+#calibrate CO2 sensor
+co2Sensor.setCalibration(.32,.16)
+print('calibration set. First reading: ', co2Sensor.ppm())
 
 client_dynamo = boto.dynamodb2.connect_to_region(
         'us-east-1',
@@ -40,14 +37,6 @@ client_dynamo = boto.dynamodb2.connect_to_region(
 table_dynamo = Table(DYNAMO_TABLE_NAME,connection=client_dynamo)
 
 
-# This function lets you run code on exit
-# def exitHandler():
-#     print("Exiting")
-#     sys.exit(0)
-
-# # Register exit handlers
-# atexit.register(exitHandler)
-# signal.signal(signal.SIGINT, SIGINTHandler)
 
 ## Read Sensor Values
 try:
@@ -65,17 +54,25 @@ print "Collecting air quality data from room " + str(room)
 ##Write to Dynamo
 while(1):
     try:
+        timestamp = str(datetime.now())
+        gas = gasSensor.read()
+        dust = dustSensor.read()
+        co2 = co2Sensor.ppm()
+        co2V = co2Sensor.volts()
+
         table_dynamo.put_item(
                        data={
-                            'timestamp':str(datetime.now()),
-                            'GasSensor':gasSensor.read(),
-                            'DustSensor': dustSensor.read(),
-                            # 'CO2': co2Sensor.ppm(),
-                            'room': str(room)
+                            'timestamp': timestamp,
+                            'GasSensor':gas,
+                            'DustSensor': dust,
+                            'CO2Sensor': co2,
+                            'room': str(room),
+                            'CO2Volts': co2V
                         })
-        print "Dust: " + str(dustSensor.read())
-        print "Gas: " + str(gasSensor.read())
-        # print "CO2: " + str(co2Sensor.ppm())
+        print "Dust: " + str(dust)
+        print "Gas: " + str(gas)
+        print "CO2: " + str(co2)
+        print "CO2V: " + str(co2V)
         time.sleep(5)
     except:
         time.sleep(5)
